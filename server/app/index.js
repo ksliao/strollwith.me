6feab3eb28da7aad082c6070bb375784f8368ec2
Albert Yin <albyin@gmail.com>
2015-06-06 17:38:39 -0400

    wired up clusterpoint api and able to insert

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,8 @@ var path = require('path');
 var express = require('express');
 var app = express();
 module.exports = app;
+var cps = require("cps-api");
+var cpsConn = require("../db").cpsConn;
 
 // Pass our express application pipeline into the configuration
 // function located at server/app/configure/index.js
@@ -30,6 +32,17 @@ app.use(function (req, res, next) {
 });
 
 app.get('/*', function (req, res) {
+    console.log("inserting to clusterpoint from app/index.js get /");
+
+    // Insert
+    var id = 3,
+       name = "Username";
+    var insert_request = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(name, "name")+'</document>');
+    cpsConn.sendRequest(insert_request, function(err, insert_response) {
+       if (err) return console.error(err);
+       console.log('New user registered: ' + insert_response.document.id);
+    });
+
     res.sendFile(app.get('indexHTMLPath'));
 });
 
