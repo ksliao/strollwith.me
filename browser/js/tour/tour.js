8ac3e3c103fc1f72b2ea4a831475e370fd884118
Tony <terimyaki@users.noreply.github.com>
2015-06-06 14:29:39 -0400

    preliminary tour structure

--- browser/js/tour/tour.js
+++ browser/js/tour/tour.js
@@ -3,19 +3,65 @@ app.config(function($stateProvider){
 	$stateProvider.state('tour', {
 		url : '/tour/:id',
 		templateUrl : 'js/tour/tour.html',
-		controller: 'TourCtrl',
-		resolve : {
-			tourData : function($stateParams, $state, TourFactory){
-				return TourFactory.findTour($stateParams.id).catch(function(){
-					$state.go('home');
-				});
-			}
-		}
+		controller: 'TourCtrl'
+		// resolve : {
+		// 	tourData : function($stateParams, $state, TourFactory){
+		// 		return TourFactory.findTour($stateParams.id).catch(function(){
+		// 			$state.go('home');
+		// 		});
+		// 	}
+		// }
 	});
 });
 
-app.controller('TourCtrl', function($scope, tourData){
-	$scope.tourData = tourData;
+app.controller('TourCtrl', function($scope){
+	$scope.tourData = {
+		creator: 'hello',
+		name : 'Katrina Euro Trip',
+		description: 'It is awesome',
+		points : [
+			{
+				latitude: 40.701929,
+				longitude: -73.983912,
+				audioUrl : '',
+				imagesUrl : [
+					'',
+					'',
+					''
+				]
+			},
+			{
+				latitude: 50.701929,
+				longitude: -83.983912,
+				audioUrl : '',
+				imagesUrl : [
+					'',
+					'',
+					''
+				]
+			},
+			{
+				latitude: 60.701929,
+				longitude: -93.983912,
+				audioUrl : '',
+				imagesUrl : [
+					'',
+					'',
+					''
+				]
+			},
+			{
+				latitude: 60.701929,
+				longitude: -93.983912,
+				audioUrl : '',
+				imagesUrl : [
+					'',
+					'',
+					''
+				]
+			}
+		]
+	};
 });
 
 app.factory('TourFactory', function($http){
@@ -42,7 +88,7 @@ app.factory('TourFactory', function($http){
 		},
 		findAllTours : function(){
 			return $http.get('/tours').then(function(response){
-
+				return response.data;
 			});
 		}
 	}
